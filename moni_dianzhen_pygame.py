a99de5b1cd30ab7f04c787f518b413a57858cc6e
YSRpointNN <94787442+YSRpointNN@users.noreply.github.com>
2022-12-20 09:29:36 +0800

    Update moni_dianzhen_pygame.py

--- moni_dianzhen_pygame.py
+++ moni_dianzhen_pygame.py
@@ -1,11 +1,11 @@
 from dianzhen_font import *
+import random
 import pygame
-import time
 SCREEN_WID = 128
 SCREEN_HEI = 64
 WIN_WID = 640
 WIN_HEI = 320
-DISPLAY_COLOR = (0, 255, 255)
+DISPLAY_COLOR = (0, 0, 0)
 #-------------------------------------------------------------------------------------------#
 def write_gram(cursor_x, cursor_y, bytes):
     if cursor_x <= SCREEN_WID and cursor_y <= SCREEN_HEI // 8:
@@ -30,30 +30,80 @@ def show_point(x, y, state):
         pygame.draw.rect(windows, (45,45,45), (dis_x, dis_y, SingleWidth-1, SingleHeight-1), 0)
 #-------------------------------------------------------------------------------------------#
 
-def ShowImg():
+def Ground():
+    global cordx
+    write_gram(cordx, 7, (0xf0 for i in range(0, 128)))
+    cordx -= 1
+    if cordx < -128:
+        cordx = SCREEN_WID
+def DinoJump():
     pass
 
+def DinoRun():
+    global secstate
+    if speed % 5 == 0:
+        secstate = not secstate
+    if secstate:
+        write_gram(3, 5, dino[0][:16])
+        write_gram(3, 6, dino[0][16:])
+    else:
+        write_gram(3, 5, dino[1][:16])
+        write_gram(3, 6, dino[1][16:])
+
+def MuenView():
+    if secstate == 66:
+        ShowString(5, 0, 'START')
+        ShowString(5, 1, 'LIST')
+        ShowString(5, 2, 'QUIT')
+    else:
+        x = 5
+    if secstate == 1:
+        ShowString(5, 1, 'LIST')
+        ShowString(5, 2, 'QUIT')
+        string = 'START'
+        y = 0
+    elif secstate == 2:
+        ShowString(5, 0, 'START')
+        ShowString(5, 2, 'QUIT')
+        string = 'LIST'
+        y = 1
+    elif secstate == 3:
+        ShowString(5, 0, 'START')
+        ShowString(5, 1, 'LIST')
+        string = 'QUIT'
+        y = 2
+    else:
+        x = 0
+    if x:
+        for char in string:
+            if x >= SCREEN_WID // 8 :
+                x = 0
+                y += 1
+            up = []
+            down = []
+            for buffer in ASCIIF8x16[char][:8]:
+                up.append(~buffer)
+            write_gram(x * 8, y * 2, up)
+            for buffer in ASCIIF8x16[char][8:]:
+                down.append(~buffer)
+            write_gram(x * 8, y * 2 + 1, down)
+            x += 1
+
 def ScreenClear():
-    clearload = (0x00 for i in range(0, SCREEN_WID * (SCREEN_HEI // 8)))
+    clearload = (0 for i in range(0, SCREEN_WID * (SCREEN_HEI // 8)))
     write_gram(0, 0, clearload)
 
-def ShowString(x, y, str):
+def ShowString(x, y, string):
     if x > SCREEN_WID // 8 or y > SCREEN_HEI // 16:
         print('Out of screen')
         return 0
-    for char in str:
+    for char in string:
         if x >= SCREEN_WID // 8 :
             x = 0
             y += 1
         write_gram(x * 8, y * 2, ASCIIF8x16[char][:8])
         write_gram(x * 8, y * 2 + 1, ASCIIF8x16[char][8:])
         x += 1
-
-def Quit():
-    global mainloop
-    mainloop = False
-    pygame.quit()
-
 #-------------------------------------------init--------------------------------------------#
 pygame.init()
 windows = pygame.display.set_mode((WIN_WID,WIN_HEI))
@@ -67,24 +117,52 @@ for i in range(0, SCREEN_WID):
 for i in range(0, SCREEN_HEI):
     pygame.draw.line(windows, (71,71,71), start_pos=(0, i*SingleWidth), end_pos=(WIN_WID, i*SingleWidth), width=1)
 pygame.display.flip()
+
+clock = pygame.time.Clock()
+speed = 0
 #------------------------------------------start--------------------------------------------#
 mainloop = True
 while mainloop:
-    write_gram(0, 1, doin[0][:16])
-    write_gram(0, 2, doin[0][16:])
-    time.sleep(0.1)
-    write_gram(0, 1, doin[1][:16])
-    write_gram(0, 2, doin[1][16:])
-    time.sleep(0.1)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            Quit()
-        if event.type ==pygame.KEYDOWN:
-            if event.key == 97:
-                ShowString(0, 0, '12')
-                ShowString(0, 1, '34')
-                time.sleep(0.5)
-            elif event.key == 100:
-                ScreenClear()
-                time.sleep(3)
-                Quit()
+    secstate = 66
+    state = 'Muen'
+    while state == 'Muen':
+        MuenView()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                state = 'Quit'
+                mainloop = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                        secstate += 1
+                        if secstate > 3:
+                            secstate = 1
+                if event.button == 3:
+                    if secstate == 1:
+                        ScreenClear()
+                        state = 'Game'
+                    if secstate == 2:
+                        ScreenClear()
+                        ShowString(0, 0, 'waiting...')
+                        state = 'List'
+                    if secstate == 3:
+                        state = 'Quit'
+                        mainloop = False
+    if state == 'Game':
+        write_gram(0, 6, (0x10 for i in range(0, SCREEN_WID)))
+        cordx = SCREEN_WID
+    while state == 'Game':
+        speed += 1
+        Ground()
+        DinoRun()
+        clock.tick(50 + (speed // 100))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                state = 'Quit'
+                mainloop = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    print('tiao')
+                    print(speed)
+                if event.button == 3:
+                    print('pa')
+pygame.quit()
